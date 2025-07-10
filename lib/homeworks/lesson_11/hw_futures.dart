3c3676f0d8439cfa5f855c31707ded7177d8d110
Vladyslav Vozniuk <voznjuckv@gmail.com>
2025-07-10 17:51:52 +0300

    feat: add Task 2 (fetchAge with proper suffix logic)

--- lib/homeworks/lesson_11/hw_futures.dart
+++ lib/homeworks/lesson_11/hw_futures.dart
@@ -1,12 +1,32 @@
 import 'dart:async';
 
-/// Task 1: Асинхронне отримання імені
+// ---------------- Task 2: Асинхронне отримання імені ---------------------
 Future<String> fetchName() async {
   await Future.delayed(Duration(seconds: 2));
   return 'Влад'; // ← заміни на своє ім’я
 }
 
+// ---------------- Task 2: Асинхронне отримання віку ---------------------
+
+Future<String> fetchAge() async {
+  await Future.delayed(Duration(milliseconds: 1500));
+  int age = 25;
+
+  String suffix;
+  if (age % 10 == 1 && age % 100 != 11) {
+    suffix = 'рік';
+  } else if ([2, 3, 4].contains(age % 10) && !(age % 100 >= 12 && age % 100 <= 14)) {
+    suffix = 'роки';
+  } else {
+    suffix = 'років';
+  }
+
+  return '$age $suffix';
+}
 void main() async {
   final name = await fetchName();
   print('Мене звати $name');
+
+  final age = await fetchAge();
+  print('Мені $age');
 }
